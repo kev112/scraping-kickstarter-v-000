9e68e40d489bff92ad1fe5f183c691165484a24b
Tao Liu <taoliudesign@gmail.com>
2018-08-26 05:17:34 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -11,7 +11,9 @@ def create_project_hash
 # projects: kickstarter.css("li.project.grid_4")
 # title: project.css("h2.bbcard_name strong a")
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
-#description: project.css("p.bbcard_blurb").text
+# description: project.css("p.bbcard_blurb").text
+# location: project.css("span.location-name").text
+# fund: project.css("ul.project-stats li.first.funded strong").text
 
   binding.pry
 end
