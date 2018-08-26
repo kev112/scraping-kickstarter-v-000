082ee1b30e8f9f0418b6278b3036bfb150e45190
Tao Liu <taoliudesign@gmail.com>
2018-08-26 03:49:27 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,13 +1,14 @@
 # require libraries/modules here
 require 'pry'
-require 'Nokogiri'
+require 'nokogiri'
+
 def create_project_hash
   # write your code here
   html = File.read('fixtures/kickstarter.html')
  
   kickstarter = Nokogiri::HTML(html)
   
-  binding.pry
+  # binding.pry
   
 end
 
